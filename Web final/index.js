c14ea3ca6f10ae2ed38a5accec7e5b712fa6ec55
juliassrnho <31696264+juliassrnho@users.noreply.github.com>
2017-12-14 18:11:53 -0500

    added files

---
+++ Web final/index.js
@@ -0,0 +1,19 @@
+   $(document).ready(function(){
+    $('.materialboxed').materialbox();
+  });
+        
+
+
+          $(document).ready(function(){
+      $('.slider').slider();
+    });
+     
+
+   }
+
+  $("blockquote").hover(function(){
+    $(this).css("background-color", yellow);
+    }, function(){
+    $(this).css("background-color", "pink");
+});
+
